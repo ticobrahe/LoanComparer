e672272da8ad65f5725d215296c0e0db7ce228c8
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-02-29 00:08:46 +0100

    update index.chtml

---
+++ LoanComparer.App/Controllers/LoanController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoanComparer.App.Controllers
+{
+    public class LoanController : Controller
+    {
+        // GET: Loan
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Loaner()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
