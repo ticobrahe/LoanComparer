3f0cdc3e31cacde709457cb0995e4cfe9c5463aa
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-02-29 11:00:05 +0100

    implement search loan provider feature

--- LoanComparer.App/Controllers/LoanController.cs
+++ LoanComparer.App/Controllers/LoanController.cs
@@ -1,24 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using LoanComparer.Data.Models.ViewModels;
+using LoanComparer.Data.Repositories.Interfaces;
 
 namespace LoanComparer.App.Controllers
 {
     public class LoanController : Controller
     {
+        private readonly ILoanRepository _loanRepository;
+
         // GET: Loan
+        public LoanController(ILoanRepository loanRepository)
+        {
+            _loanRepository = loanRepository;
+        }
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
+        [HttpGet]
         public ActionResult Loaner()
         {
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult>Loaner(HomeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["amount"] = model.Amount;
+                Session["duration"] = model.Duration;
+                var loaners = await _loanRepository.FindLoaner(model);
+                ViewBag.Count = loaners.Count();
+                return View(loaners);
+            }
+            return View();
         }
     }
 }
--- LoanComparer.Data/Models/ViewModels/LoanerViewModel.cs
+++ LoanComparer.Data/Models/ViewModels/LoanerViewModel.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LoanComparer.Data.Models.ViewModels
 {
-    public class HomeViewModel
+    public class LoanerViewModel
     {
-        [Required]
-        public decimal Amount { get; set; }
-        [Required]
-        public int Duration { get; set; }
-        [Required]
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
         public string LoanType { get; set; }
     }
 }
