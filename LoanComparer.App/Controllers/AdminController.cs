80887e3a00ff6ba494936a3064ad923c29c2bf2e
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-02 19:18:55 +0100

    add admin controler and admin view

---
+++ LoanComparer.App/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoanComparer.App.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult CreateProvider()
+        {
+            return View();
+        }
+
+    }
+}
