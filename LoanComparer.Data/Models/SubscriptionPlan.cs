b6839bfab958a9fdadded311764abb9f344efc87
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-09 18:06:34 +0100

    created SubscriptionPlan model

---
+++ LoanComparer.Data/Models/SubscriptionPlan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanComparer.Data.Models
+{
+    public class SubscriptionPlan
+    {
+        public int Basic { get; set; }
+        public int Premium { get; set; }
+        public int Ultimate { get; set; }
+    }
+}
