9a0447310e0f0823eefa428cda0110416bad5d95
Olabiyi Samuel <olabiyi.sam@gmail.com>
2020-03-10 10:22:03 +0100

    create loan provider view model

---
+++ LoanComparer.App/Models/CreateProviderViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LoanComparer.App.Models
+{
+    public class CreateProviderViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LoanType { get; set; }
+        [Required]
+        public decimal MinimumAmount { get; set; }
+        [Required]
+        public decimal MaximumAmount { get; set; }
+        [Required]
+        public int Duration { get; set; }
+        [Required]
+        public string ProviderLink { get; set; }
+
+        [Required]
+        public string Terms { get; set; }
+        [Required]
+        public decimal Rate { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+    }
+}
